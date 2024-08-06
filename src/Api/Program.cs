e0e7488f1e066f71e5c4af4feebb6eb6d51560b3
Gustavo Cuentas <gustavo.cuentas@culqi.com>
2024-08-05 21:27:01 -0500

    primera clase

---
+++ src/Api/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configurar el pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
